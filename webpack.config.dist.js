bfc3f2c52cdf40517789c85050879d37694b3b9f
alexeychikk <alexzinkevych@yandex.ua>
2016-04-06 11:53:35 +0300

    Bundle build npm scripts

---
+++ webpack.config.dist.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: { warnings: false }
+        })
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                include: path.join(__dirname, 'src'),
+                exclude: /node_modules/,
+                loaders: ['react-hot', 'babel']
+            },
+            {
+                test: /(\.css|\.scss)$/,
+                loaders: ['style', 'css?sourceMap', 'sass?sourceMap']
+            },
+            {
+                test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
+                loader: 'url-loader?limit=100000'
+            }
+        ]
+    }
+};
