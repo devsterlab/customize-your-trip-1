aba244a898c1b3ee9422ed4c6085ff73c5ebef1f
tihomirov <assatihomirov9@gmail.com>
2016-04-07 10:57:57 +0300

    init data base script

--- server/app.js
+++ server/app.js
@@ -1,16 +1,32 @@
 var express = require('express');
-var http = require('http');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var path = require('path');
+
 var config = require('./config');
 
-var app = express();
-
-app.use(express.static(path.join(__dirname, '../')));
-
-http.createServer(app).listen(config.get('port'), 'localhost', function (err, result) {
+server.listen(config.get('port'), 'localhost', function (err, result) {
 	if (err) {
 		console.log(err);
 	}
 
 	console.log('Listening at localhost:' + config.get('port'));
+});
+
+app.use(express.static(path.join(__dirname, '../')));
+
+io.on('connection', function (socket) {
+	socket.emit('news', { hello: 'world' });
+	socket.on('my other event', function (data) {
+		console.log(data);
+	});
+
+	var City = require('./models/city').City;
+
+	City.find({}, function (err, cities) {
+		if (err) throw err;
+		socket.emit('cities', cities);;
+	})
+
 });
